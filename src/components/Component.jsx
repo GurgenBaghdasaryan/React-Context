44c13bead2b3fc1f6df20efb732748f54fc6f5c6
GurgenBaghdasaryan <grg.baghdasaryan@gmail.com>
2021-01-12 21:08:14 +0400

    Context

---
+++ src/components/Component.jsx
@@ -0,0 +1,52 @@
+import React, { useContext, useState } from "react";
+import { LoginContext } from "../App";
+
+const Component = () => {
+  const { setIsLogin, isLogin } = useContext(LoginContext);
+  const [loginField, setLoginField] = useState("");
+  const [passwordField, setPasswordField] = useState("");
+
+  const checkLogin = () => {
+    if (
+      loginField == "" ||
+      loginField == " " ||
+      passwordField == "" ||
+      passwordField == " "
+    ) {
+      return;
+    }
+
+    setIsLogin((prevLogin) => !prevLogin);
+  };
+
+  if (!isLogin) {
+    return (
+      <>
+        <h1>Hello</h1>
+        <button type="checkbox" onClick={checkLogin}>
+          Exit
+        </button>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <input
+        onChange={(e) => setLoginField(e.target.value)}
+        type="text"
+        placeholder="Enter Login"
+      ></input>
+      <input
+        onChange={(e) => setPasswordField(e.target.value)}
+        type="password"
+        placeholder="Enter Password"
+      ></input>
+      <button type="checkbox" onClick={checkLogin}>
+        Enter
+      </button>
+    </>
+  );
+};
+
+export default Component;
